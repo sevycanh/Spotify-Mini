1b7af70d6b50b24f9aff3b87ba3d02fa03d185c2
DecadeXD <maicanh2002@gmail.com>
2024-07-23 23:31:54 +0700

    UI Splash - Get Started - Choose Mode
    Splash Page - Get Started Page - Choose Mode Page

---
+++ app/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:app/core/configs/theme/app_theme.dart';
+import 'package:app/presentation/splash/pages/splash.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
+  await initializeDependencies();
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit())
+      ],
+      child: BlocBuilder<ThemeCubit,ThemeMode>(
+        builder: (context,mode) => MaterialApp(
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: mode,
+          debugShowCheckedModeBanner: false,
+          home: const SplashPage()
+        ),
+      ),
+    );
+  }
+}
